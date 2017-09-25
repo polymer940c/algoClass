853597bde09f1dbb174f36dfa095a0604a73c8a7
kevin hu <hu.kevin.w@gmail.com>
2017-09-25 18:32:12 -0400

    Update factorial.js

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,16 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+function factorial(num) {
+7
+  if (num === 1 ) {
+8
+    return 1
+9
+  } else {
+10
+    return num * factorial( num - 1 )
+11
+  }
+12
+}
